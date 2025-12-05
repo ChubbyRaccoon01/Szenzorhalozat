5283826a1075aac012408aeef809d9bfcf44ba78
Simon Peter <simonpetymeg@gmail.com>
2025-12-05 13:33:11 +0000

    valami nem jó..

--- Szenzorhalozat.Console/Program.cs
+++ Szenzorhalozat.Console/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szenzorhalozat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sensors()> szenzorok = new List<Sensors>();
+            szenzorok.Add(new Szenzor("Szenzor1", 10.5, "Celsius"));
+            szenzorok.Add(new Szenzor("Szenzor2", 22.3, "Celsius"));
+            
+            foreach (var szenzor in szenzorok)
+            {
+                Console.WriteLine(szenzor);
+            }
+        }
+    }
+}
