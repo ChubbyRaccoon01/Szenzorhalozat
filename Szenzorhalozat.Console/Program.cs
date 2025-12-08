f051200b34872098a0e8f00c20c28ea29a967803
Simon Peter <simonpetymeg@gmail.com>
2025-12-08 20:17:51 +0100

    semmi hasznos

--- Szenzorhalozat.Console/Program.cs
+++ Szenzorhalozat.Console/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Sensors;
 
 namespace Szenzorhalozat
 {
@@ -11,19 +10,14 @@ namespace Szenzorhalozat
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             List<Sensor> szenzorok = new List<Sensor>();
             szenzorok.Add(new TemperatureSensor());
             szenzorok.Add(new TemperatureSensor());
-            
+
             foreach (var szenzor in szenzorok)
             {
-                Console.WriteLine(szenzor);
+                Console.WriteLine(szenzor.ToString());
             }
-=======
-            TemperatureSensor sensor = new TemperatureSensor();
-            Console.WriteLine(sensor.ValueUpd());
->>>>>>> 3db0e45 (?)
         }
     }
 }
